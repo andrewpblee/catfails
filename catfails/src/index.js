9fd61f7c4b8e639a4d1cde85ff6e6c3155488072
andrewpblee <andrewpblee@gmail.com>
2019-06-18 20:36:41 +0100

    solving loading issue

--- catfails/src/index.js
+++ catfails/src/index.js
@@ -1,12 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+import { css } from "@emotion/core";
+import { ScaleLoader } from "react-spinners";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const apiKey = "QYgzjYAhVqhzr08lQkKtnjYGXadZx7I4";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const override = css`
+  display: block;
+  margin: 0 auto;
+`;
+
+class App extends React.Component {
+  state = {
+    gif: "",
+    loading: true
+  };
+
+  getGif = async () => {
+    const response = await fetch(
+      `http://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=catfail`
+    );
+    const blob = await response.json();
+    const gif = await blob.data.images.original;
+    this.setState({
+      gif: gif.url,
+      loading: true
+    });
+  };
+
+  onButtonclick = async () => {
+    this.setState({
+      loading: true
+    });
+    await this.getGif();
+  };
+
+  async componentDidMount() {
+    await this.getGif();
+  }
+
+  render() {
+    const { loading, gif } = this.state;
+    return (
+      <Fragment>
+        <div className="container">
+          {loading ? (
+            <div className="sweet-loading">
+              <ScaleLoader
+                css={override}
+                sizeUnit={"px"}
+                size={150}
+                color={"#ff6f61"}
+                loading={loading}
+              />
+            </div>
+          ) : (
+            <img src={gif} alt="cute cat" />
+          )}
+        </div>
+        <button onClick={this.onButtonclick}>New Fail</button>
+      </Fragment>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
